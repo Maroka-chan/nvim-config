a3577c0240540db56deaebf200b5b44ebfea9d95
Maroka-chan <64618598+Maroka-chan@users.noreply.github.com>
2026-02-27 13:05:09 +0100

    use stable neovim

--- flake.nix
+++ flake.nix
@@ -1,42 +1,26 @@
 {
-  description = "Neovim Configuration Flake";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
-  };
+  outputs = {nixpkgs, ...}: let
+    supportedSystems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    forAllSystems = function:
+      nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          function nixpkgs.legacyPackages.${system}
+      );
+  in rec {
+    packages = forAllSystems (pkgs: {
+      default = pkgs.callPackage ./neovim.nix {};
+    });
 
-  outputs =
-    { nixpkgs, neovim-nightly, ... }:
-    let
-      supportedSystems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          function (
-            import nixpkgs {
-              inherit system;
-              overlays = [
-                neovim-nightly.overlays.default
-              ];
-            }
-          )
-        );
-    in
-    rec {
-      packages = forAllSystems (pkgs: {
-        default = pkgs.callPackage ./neovim.nix { };
-      });
-
-      nixosModules = rec {
-        custom-neovim = import ./module.nix packages;
-        default = custom-neovim;
-      };
+    nixosModules = rec {
+      custom-neovim = import ./module.nix packages;
+      default = custom-neovim;
     };
+  };
 }
